d5fbaaad536334d7a50554453adb999c262e7bf8
Marylene <baeyens.ma@gmail.com>
2024-02-03 18:08:54 +0100

    Table-Course

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormCourseComponent } from './Course/form-course/form-course.component';
+import { TableCourseComponent } from './Course/table-course/table-course.component';
 
 const routes: Routes = [
   {path:"form-course", component:FormCourseComponent},
-  {path:"form-course/:name", component:FormCourseComponent}
+  {path:"form-course/:name", component:FormCourseComponent},
+  {path:"table-course", component:TableCourseComponent}
 ];
 
 @NgModule({
